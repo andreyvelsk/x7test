9d89f48185680dfd21fee59fbd31cb5a772ce97e
andrey_velsk <andrey_velsk@mail.ru>
2020-02-02 15:25:50 +0300

    add comments

--- header.php
+++ header.php
@@ -1,3 +1,23 @@
+<?php
+require_once "dbconnect.php";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:600|Playfair+Display:700|Roboto:400,700" rel="stylesheet">
+    <!-- Add the slick-theme.css if you want default styling -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <!-- Add the slick-theme.css if you want default styling -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+</head>
+<body>
+
 <nav class="top-navbar">
     <div class="container">
         <div class="row mainnavbar">
--- post.php
+++ post.php
@@ -1,22 +1,3 @@
-<?php
-require_once "dbconnect.php";
-?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:600|Playfair+Display:700|Roboto:400,700" rel="stylesheet">
-    <!-- Add the slick-theme.css if you want default styling -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-    <!-- Add the slick-theme.css if you want default styling -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
-</head>
-<body>
 <?php 
 include_once "header.php"; 
 
@@ -52,6 +33,36 @@ $sql = "select * from posts WHERE id = :id";
                     </div>
                     <!--/content-->
                 </div>
+
+                <h2>Комментарии</h2>
+                <?php
+                    $sql = "select c.*, u.email from comments c LEFT JOIN users u ON c.iduser = u.id WHERE idpost = :idpost";
+                    if ($stmt = $conn->prepare($sql)) {
+                        $stmt->bindValue(':idpost', (int)$postid);
+                        $stmt->execute();
+                        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                        if ($rows != null) {
+                            foreach ($rows as $key => $row) {
+                                ?>
+                                    <div class="comment">
+                                        <div class="comment-item">
+                                            <div class="comment-text">
+                                                <?= $row['commenttext']?>
+                                            </div>
+                                            <div class="comment-meta">
+                                            <?= $row['email']?>, <?= $row['commentdate']?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                            }
+                
+                        } else {
+                            echo "no comments";
+                        }
+                    }
+
+                ?>
             </div>
 
             <!--page-->
